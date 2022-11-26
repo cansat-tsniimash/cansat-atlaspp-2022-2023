e08471e97d97056d0e3397f87e4f1ffc7d7f82ec
Intes1k <yubko.tema@gmail.com>
2022-11-26 19:09:00 +0300

    Добавлены фоторезистор и DS18, DS18 не работает

--- src/board/Cerberus/application/app_main.c
+++ src/board/Cerberus/application/app_main.c
@@ -9,8 +9,11 @@
 #include "stm32f4xx.h"
 #include <LSM6DS3/DLSM.h>
 #include <LIS3MDL/DLIS3.h>
+#include <Photorezistor/photorezistor.h>
+#include <1Wire_DS18B20/one_wire.h>
 
 extern SPI_HandleTypeDef hspi2;
+extern ADC_HandleTypeDef hadc1;
 
 int app_main(){
 	/*инициализация sr датчика*/
@@ -33,6 +36,7 @@ int app_main(){
 	struct bme280_dev bme;
 	bme_init_default_sr(&bme, &bme_struct);
 
+	//стх и структура лcмa
 	stmdev_ctx_t ctx_lsm;
 
 	struct lsm_spi_intf_sr lsm_sr;
@@ -41,6 +45,7 @@ int app_main(){
 	lsm_sr.sr = &shift_reg_n;
 	lsmset_sr(&ctx_lsm, &lsm_sr);
 
+	//стх и структура лиса
 	stmdev_ctx_t ctx_lis;
 
 	struct lis_spi_intf_sr lis_sr;
@@ -49,23 +54,54 @@ int app_main(){
 	lis_sr.sr = &shift_reg_n;
 	lisset_sr(&ctx_lis, &lis_sr);
 
+	//структура фоторезистора
+	photorezistor_t photrez;
+	photrez.resist = 2000;
+	photrez.hadc = &hadc1;
+
+	//структура пин оне ваера
+	ds18b20_t ds;
+	ds.onewire_port = GPIOA;
+	ds.onewire_pin = GPIO_PIN_1;
+
+	//Init оф ван ваер
+	onewire_init(&ds);
+	ds18b20_set_config(&ds, 100, -100, DS18B20_RESOLUTION_12_BIT);
+
+	//переменные
 	float temperature_celsius_gyro = 0.0;
 	float acc_g[3] = {0};
 	float gyro_dps[3] = {0};
 	float temperature_celsius_mag = 0.0;
 	float mag[3] = {0};
+	uint16_t temp_ds;
+	bool crc_ok_ds = false;
+	uint32_t start_time_ds = HAL_GetTick();
 
-	HAL_Delay(100);
+	//структура бме даты
 	struct bme280_data bme_data;
 	bme_data = bme_read_data(&bme);
+	//давление на земле
 	double ground_pressure = bme_data.pressure;
 
+	ds18b20_start_conversion(&ds);
+
 	while(1){
+		//данные в беск цикле
 		bme_data = bme_read_data(&bme);
 		double pressure = bme_data.pressure;
 		double height = 44330 * (1 - pow(pressure / ground_pressure, 1.0 / 5.255));
+		float lux = photorezistor_get_lux(photrez);
 		lsmread(&ctx_lsm, &temperature_celsius_gyro, &acc_g, &gyro_dps);
 		lisread(&ctx_lis, &temperature_celsius_mag, &mag);
+		//каждые 750 мс берет температуру
+		if (HAL_GetTick()-start_time_ds >= 750)
+		{
+			ds18b20_read_raw_temperature(&ds, &temp_ds, &crc_ok_ds);
+			ds18b20_start_conversion(&ds);
+			start_time_ds = HAL_GetTick();
+			temp_ds /= 16;
+		}
 	}
 	return 0;
 }
