b1300288176553633348b9a9e4d41cba1144d8b6
Intes <yubko.tema@gmail.com>
2023-04-01 19:23:04 +0300

    Написали функцию рид

--- src/board/Cerberus/application/bb.c
+++ src/board/Cerberus/application/bb.c
@@ -67,18 +67,18 @@ int bb_chip_err()
 
 
 
-//void bb_read(uint32_t addr, uint8_t* buf, uint8_t size)
-//{
-/**
- uint8_t cmd = I2C_LINK_CMD_SET_PACKET;
+int bb_read(uint32_t addr, uint8_t* buf, uint8_t size)
+{
+	uint8_t cmd = I2C_LINK_CMD_SET_PACKET;
+ 	uint16_t size_mes;
 	i2c_pack_t i2c_pack;
 	i2c_pack.num = CMD_3;
 	i2c_pack.size = 5;
 	memcpy(i2c_pack.data, &addr, 4);
 	if (size > 32)
-		data[4] = 32;
+		i2c_pack.data[4] = 32;
 	else
-		data[4] = size;
+		i2c_pack.data[4] = size;
 	HAL_StatusTypeDef rc = HAL_I2C_Master_Transmit(&hi2c1, 0x77<<1, &cmd, sizeof(cmd), 1);
 	if (rc == HAL_BUSY)
 	{
@@ -97,14 +97,51 @@ int bb_chip_err()
 	{
 		return rc;
 	}
-	return rc;
 
 	HAL_Delay(10);
-
- */
-//	uint8_t cmd = I2C_LINK_CMD_SET_PACKET;
-//	i2c_pack_t i2c_pack;
-//}
+	cmd = I2C_LINK_CMD_GET_SIZE;
+	rc = HAL_I2C_Master_Transmit(&hi2c1, 0x77<<1, &cmd, 1, 1);
+	if (rc == HAL_BUSY)
+	{
+		I2C_ClearBusyFlagErratum(&hi2c1, 100);
+	}
+	if (rc != HAL_OK)
+	{
+		return rc;
+	}
+	rc = HAL_I2C_Master_Receive(&hi2c1, 0x77<<1, (uint8_t *)&size_mes, 2, 1);
+	if (rc == HAL_BUSY)
+	{
+		I2C_ClearBusyFlagErratum(&hi2c1, 100);
+	}
+	if (rc != HAL_OK)
+	{
+		return rc;
+	}
+	cmd = I2C_LINK_CMD_GET_PACKET;
+	rc = HAL_I2C_Master_Transmit(&hi2c1, 0x77<<1, &cmd, 1, 1);
+	if (rc == HAL_BUSY)
+	{
+		I2C_ClearBusyFlagErratum(&hi2c1, 100);
+	}
+	if (rc != HAL_OK)
+	{
+		return rc;
+	}
+	if(size_mes >= size)
+		rc = HAL_I2C_Master_Receive(&hi2c1, 0x77<<1, buf, size, 1);
+	else
+		rc = HAL_I2C_Master_Receive(&hi2c1, 0x77<<1, buf, size_mes, 1);
+	if (rc == HAL_BUSY)
+	{
+		I2C_ClearBusyFlagErratum(&hi2c1, 100);
+	}
+	if (rc != HAL_OK)
+	{
+		return rc;
+	}
+	return rc;
+}
 
 int bb_write(uint32_t addr, uint8_t* buf, uint8_t size)
 {
