0a76c2964914b8890e618b5c887934c7bce92f3c
Agnus290 <tenebris.agnus@yandex.ru>
2023-05-27 18:06:52 +0300

    Альфа

---
+++ src/researches/Alpha/Application/app_main.c
@@ -0,0 +1,244 @@
+/*
+ * Application.c
+ *
+ *  Created on: Jan 14, 2023
+ *      Author: Install
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "main.h"
+
+
+#include "drivers_i2c/Inc/its-i2c-link.h"
+#include <MX25L512_/MX25L512.h>
+
+extern I2C_HandleTypeDef hi2c1;
+
+
+uint8_t flag = 16;
+#pragma pack(push,1)
+
+typedef enum
+{
+	    CMD_BUZ = 0x31,
+	    //управление пьезодинамиком
+	    CMD_CE = 0x32,
+	    //очистка памяти
+	    CMD_Read = 0x33,
+	    //чтение ПАМЯТИ
+	    CMD_Write = 0x34,
+	    //запись данных
+	    CMD_ReadADDR = 0x35,
+	    //Чтение по АДРЕСУ
+	    CMD_Continue = 0x36,
+	    //Продолжаю
+	    CMD_CE_GPS = 0x37,
+	    //выключить питание
+	    CMD_OFF = 0x38,
+	    //gps read
+	    CMD_Read_gps = 0x39,
+	    //запись бита полета ёмаё | write fly's bit
+	    CMD_Write_flys_bit = 0x40,
+	    //отправка по радио эсть жэ || radio send
+	    CMD_Radio_send = 0x41,
+	    //отправка по радио записаного сообщения
+	    CMD_Radio_send_d = 0x42,
+	    //настройка радио
+	    CMD_Settings = 0x43
+	}cmd_t;
+
+uint8_t buf[30];
+
+typedef struct
+{
+	uint8_t num;
+	uint8_t size;
+	uint8_t data[36];
+} cmd_pack_t;
+
+typedef struct
+{
+	uint8_t flag;
+	uint16_t num;
+	uint16_t time_s;
+	float lat;
+	float lon;
+	float alt;
+	uint32_t gps_time_s;
+	uint32_t gps_time_us;
+	int8_t fix;
+	uint16_t crc;
+} nrf_pack_t;
+
+typedef struct
+{
+	nrf24_data_rate_t data_rate;
+	nrf24_tx_power_t tx_power;
+	uint8_t rf_channel;
+	nrf24_crc_size_t crc_size;
+	nrf24_address_width_t address_width;
+	bool en_dyn_payload_size;
+	bool en_ack_payload;
+	bool en_dyn_ack;
+	uint64_t tx_channel;
+	//мб удалить
+	uint8_t auto_retransmit_cout;
+	uint8_t auto_retransmit_delay;
+}settings_pack_t;
+
+#pragma pack(pop)
+
+
+
+
+int app_main(){
+	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
+
+	its_i2c_link_start();
+
+//variables
+
+//settings
+
+
+
+
+
+	int nrf_irq;
+	uint32_t start_time_nrf = HAL_GetTick();
+	cmd_pack_t pack;
+	nrf_pack_t nrf_pack = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	uint8_t Data[3];
+	uint32_t addr;
+	int fix_;
+	int64_t cookie;
+	uint64_t time_s_;
+	uint32_t time_us_;
+	float lat;
+	float lon;
+	float alt;
+
+
+
+	settings_pack_t settings_pack;
+	while(1){
+		//mx25l512_rdid(&bus_data, Data);
+		//HAL_Delay(10);
+
+		HAL_Delay(1);
+		//shift_reg_write_bit_8(&shift_reg, 7, 1);
+		//HAL_Delay(100);
+		//shift_reg_write_bit_8(&shift_reg, 7, 0);
+		//HAL_Delay(100);
+		//const char hello[] = "hello i'm a bus";
+		//int rrc = its_i2c_link_write(hello, sizeof(hello));
+
+		int rc = its_i2c_link_read(&pack, sizeof(pack));
+		if (rc > 0)
+		{
+			switch(pack.num){
+				case CMD_BUZ:
+					if (pack.size == 1)
+					{
+						if (pack.data[0])
+							shift_reg_write_bit_8(&shift_reg, 7, 1);//Вкл
+						else
+							shift_reg_write_bit_8(&shift_reg, 7, 0);//Выкл
+					}
+					break;
+				case CMD_CE:
+					if (pack.size == 0)
+						mx25l512_CE(&bus_data);//Затираю чип целиком
+					break;
+
+				case CMD_ReadADDR:
+					if ((pack.size = 5) && (pack.data[4] <= 32))
+					{
+						uint32_t addr = pack.data[0] | pack.data[1] << 8 | pack.data[2] << 16 | pack.data[3] << 24;
+						uint8_t size = pack.data[4];
+						mx25l512_read(&bus_data, &addr, pack.data, size);//читаю данные
+						pack.size = size;
+						its_i2c_link_write(&pack, sizeof(pack));
+					}
+					break;
+				case CMD_Write:
+					if (pack.size <= 32)
+					{
+						//uint32_t addr = pack.data[0] | pack.data[1] << 8 | pack.data[2] << 16 | pack.data[3] << 24;
+						//mx25l512_PP(&bus_data, &addr, pack.data + 4, pack.size - 4);//Записываю данные
+					}
+					break;
+				case CMD_Read:
+					if (pack.size == 1 && pack.data[0] <= 32)
+					{
+						addr = 0x0000;
+						uint8_t size = pack.data[0];
+						mx25l512_read(&bus_data, &addr, pack.data, size);
+						addr = size << 4;
+						its_i2c_link_write(&pack, sizeof(pack));
+
+					}
+				case CMD_Continue:
+					if (pack.size == 1 && pack.data[0] <= 32)
+					{
+						uint8_t size = pack.data[0];
+						uint8_t new_addr = addr + (size << 4);
+						if (addr && (0x0f << 12) != new_addr && (0x0f << 12))
+						{
+							addr = new_addr && (0x0f << 12);
+							new_addr = addr + (sizeof(pack) << 4);
+						}
+						mx25l512_read(&bus_data, &addr, pack.data, size);
+						addr = new_addr;
+						its_i2c_link_write(&pack, sizeof(pack));
+					}
+				case CMD_OFF:
+					if (pack.size == 0)
+					{
+						shift_reg_write_bit_8(&shift_reg, 6, 0);
+					}
+				case CMD_Read_gps:
+					if (pack.size == 2)
+					{
+
+						uint8_t num = pack.num;
+						uint32_t addr = (num % 9* 28) << 4 | (num / 9) << 12;
+						mx25l512_read(&bus_data, &addr, pack.data, 28);
+					}
+				case CMD_Write_flys_bit:
+					if (pack.size == 1)
+					{
+
+					}
+
+
+
+
+					if(pack.size == 0)
+					{
+						nrf24_fifo_write(&nrf24, buf, sizeof(buf), false);
+					}
+
+
+
+
+
+
+
+
+					}
+
+				}
+			}
+
+
+
+
+
+
+
+}
---
+++ src/researches/Alpha/Core/Src/main.c
@@ -0,0 +1,334 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  LL_I2C_InitTypeDef I2C_InitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  /**I2C1 GPIO Configuration
+  PA9   ------> I2C1_SCL
+  PA10   ------> I2C1_SDA
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_9;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_10;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_4;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_I2C1);
+
+  /* I2C1 DMA Init */
+
+  /* I2C1_RX Init */
+  LL_DMA_SetDataTransferDirection(DMA1, LL_DMA_CHANNEL_3, LL_DMA_DIRECTION_PERIPH_TO_MEMORY);
+
+  LL_DMA_SetChannelPriorityLevel(DMA1, LL_DMA_CHANNEL_3, LL_DMA_PRIORITY_LOW);
+
+  LL_DMA_SetMode(DMA1, LL_DMA_CHANNEL_3, LL_DMA_MODE_NORMAL);
+
+  LL_DMA_SetPeriphIncMode(DMA1, LL_DMA_CHANNEL_3, LL_DMA_PERIPH_NOINCREMENT);
+
+  LL_DMA_SetMemoryIncMode(DMA1, LL_DMA_CHANNEL_3, LL_DMA_MEMORY_INCREMENT);
+
+  LL_DMA_SetPeriphSize(DMA1, LL_DMA_CHANNEL_3, LL_DMA_PDATAALIGN_BYTE);
+
+  LL_DMA_SetMemorySize(DMA1, LL_DMA_CHANNEL_3, LL_DMA_MDATAALIGN_BYTE);
+
+  /* I2C1_TX Init */
+  LL_DMA_SetDataTransferDirection(DMA1, LL_DMA_CHANNEL_2, LL_DMA_DIRECTION_MEMORY_TO_PERIPH);
+
+  LL_DMA_SetChannelPriorityLevel(DMA1, LL_DMA_CHANNEL_2, LL_DMA_PRIORITY_LOW);
+
+  LL_DMA_SetMode(DMA1, LL_DMA_CHANNEL_2, LL_DMA_MODE_NORMAL);
+
+  LL_DMA_SetPeriphIncMode(DMA1, LL_DMA_CHANNEL_2, LL_DMA_PERIPH_NOINCREMENT);
+
+  LL_DMA_SetMemoryIncMode(DMA1, LL_DMA_CHANNEL_2, LL_DMA_MEMORY_INCREMENT);
+
+  LL_DMA_SetPeriphSize(DMA1, LL_DMA_CHANNEL_2, LL_DMA_PDATAALIGN_BYTE);
+
+  LL_DMA_SetMemorySize(DMA1, LL_DMA_CHANNEL_2, LL_DMA_MDATAALIGN_BYTE);
+
+  /* I2C1 interrupt Init */
+  NVIC_SetPriority(I2C1_IRQn, 0);
+  NVIC_EnableIRQ(I2C1_IRQn);
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  /** I2C Initialization
+  */
+  LL_I2C_DisableOwnAddress2(I2C1);
+  LL_I2C_DisableGeneralCall(I2C1);
+  LL_I2C_EnableClockStretching(I2C1);
+  I2C_InitStruct.PeripheralMode = LL_I2C_MODE_I2C;
+  I2C_InitStruct.Timing = 0x2000090E;
+  I2C_InitStruct.AnalogFilter = LL_I2C_ANALOGFILTER_ENABLE;
+  I2C_InitStruct.DigitalFilter = 0;
+  I2C_InitStruct.OwnAddress1 = 238;
+  I2C_InitStruct.TypeAcknowledge = LL_I2C_ACK;
+  I2C_InitStruct.OwnAddrSize = LL_I2C_OWNADDRESS1_7BIT;
+  LL_I2C_Init(I2C1, &I2C_InitStruct);
+  LL_I2C_EnableAutoEndMode(I2C1);
+  LL_I2C_SetOwnAddress2(I2C1, 0, LL_I2C_OWNADDRESS2_NOMASK);
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel2_3_IRQn interrupt configuration */
+  NVIC_SetPriority(DMA1_Channel2_3_IRQn, 0);
+  NVIC_EnableIRQ(DMA1_Channel2_3_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(latch_GPIO_Port, latch_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_Pin */
+  GPIO_InitStruct.Pin = LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : latch_Pin */
+  GPIO_InitStruct.Pin = latch_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(latch_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
